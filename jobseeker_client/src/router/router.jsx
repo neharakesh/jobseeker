9f733d1252b1c41df909bf637dba9496918ec6cf
Neha Rajpoot <neharajpoot233@gmail.com>
2024-09-13 09:30:42 +0530

    basic data nd basic css is added

--- jobseeker_client/src/router/router.jsx
+++ jobseeker_client/src/router/router.jsx
@@ -7,8 +7,7 @@ const router = createBrowserRouter([
       element:<App/>,
       children:[
         {
-            path:"/",
-            element:<Home/>
+            path:"/",element:<Home/>
         }
       ]
     },
