f0a78d74a06098a49a07737b4191365749e1af91
Neha Rajpoot <neharajpoot233@gmail.com>
2024-09-16 18:52:29 +0530

    location and pagination is done in left side of main page

--- jobseeker_client/src/pages/home.jsx
+++ jobseeker_client/src/pages/home.jsx
@@ -3,14 +3,20 @@ import Jobs from '../pages/jobs.jsx'
 import Banner from '../components/banner'
 import { useState } from 'react';
 import Cards from '../components/cards.jsx';
+import Sidebar from '../sidebar/sidebar.jsx';
 
 function Home() {
     const[selectedCategories,setSelectedCategories]=useState(null)
     const [jobs,setJobs]=useState([])
+    const[isLoading,setIsLoading]=useState(true)
+    const [currentPage,setCurrentPage]=useState(1)
+    const itemsPerPage=6;
 
     useEffect(()=>{
+        setIsLoading(true);
         fetch("jobs.json").then(res=>res.json()).then(data=>{//console.log(data)
             setJobs(data)
+            setIsLoading(false)
         })
     },[])
     //handleinput change
@@ -34,6 +40,27 @@ function Home() {
         selectedCategories(event.target.value)
     }
 
+    //calculate the index range
+    const calculatePageRange=()=>{
+        const startIndex=(currentPage-1) * itemsPerPage;
+        const endIndex=startIndex+itemsPerPage;
+        return {startIndex,endIndex}
+    }
+
+    //function for next page
+    const nextPage=()=>{
+        if(currentPage<Math.ceil(filterItems.length/itemsPerPage))
+            setCurrentPage(currentPage+1)
+    }
+    //function for previous page
+    const prevPage=()=>{
+        if(currentPage>1)
+        {
+            setCurrentPage(currentPage-1)
+        }
+    }
+
+
     //main function
     const filterData=(jobs,selected,query)=>{
         let filterJobs=jobs;
@@ -45,14 +72,19 @@ function Home() {
         }
         //categories filtering
         if(selected){
-            filterJobs=filterJobs.filter(({jobLoaction,maxPrice,experienceLevel,salaryType,employmentType,PostingDate})=>(
-                jobLoaction.toLowerCase()===selected.toLowerCase() || parseInt(maxPrice)<= parseInt(selected) ||
+            filterJobs=filterJobs.filter(({jobLocation,maxPrice,experienceLevel,salaryType,employmentType,PostingDate})=>(
+                jobLocation.toLowerCase()===selected.toLowerCase() || parseInt(maxPrice)<= parseInt(selected) ||
                 salaryType.toLowerCase()===selected.toLowerCase() ||
                 employmentType.toLowerCase()===selected.toLowerCase()
 
             ))
             console.log(filterJobs)
         }
+
+
+        //slice the data based on curent page
+        const {startIndex,endIndex}=calculatePageRange()
+        filterJobs=filterJobs.slice(startIndex,endIndex)
         return filterJobs.map((data,i)=> <Cards key={i} data={data} />)
     }
 
@@ -64,14 +96,35 @@ function Home() {
 
         {/*main context */}
         <div className='bg-slate-100  flex md:grid grid-cols-4 gap-8 lg:px-24 py-12'>
-        <div className='bg-white p-4 rounded '>left</div>
+        {/*Left side */}
+        <div className='bg-white p-4 rounded '><Sidebar handleChange={handleChange} handleClick={handleClick} /></div>
+        {/*Job cards */}
         <div className='col-span-2 bg-white'>
-                <Jobs result={result}/>
+            {
+                isLoading?(<p className='font-medium'>Loading....</p>):result.length>0?(<Jobs result={result}/>):<>
+                <h3 className='text-lg font-bold mb-2'>{result.length} Jobs</h3>
+                <p>No Data Found</p>
+                </>
+            }
+
+            {/*Pagination  */}
+            {
+                result.length>0?(
+                    <div className='flex justify-center mt-4 space-x-8 mb-4'>
+                        <button onClick={prevPage}>Previous</button>
+                        <button >Page {currentPage} of {Math.ceil(filterItems.length/itemsPerPage)}</button>
+                        <button onClick={nextPage}>Next</button>
+
+                    </div>
+                ):""
+            }
+                
             </div>
         <div className='bg-white p-4 rounded '>right</div>
         </div>
-            
         </div>
+            
+        
     )
 }
 
--- jobseeker_client/src/pages/jobs.jsx
+++ jobseeker_client/src/pages/jobs.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 
 function Jobs({result}) {
     return (
+        <>
         <div>
-            {result}
+            <h3 className='text-lg font-bold mb-2'>{result.length}  Jobs</h3>
+            
         </div>
+        <section className='card-container'>{result}
+
+        </section>
+        </>
     )
 }
 
